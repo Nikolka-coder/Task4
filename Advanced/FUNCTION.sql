bc4cc6b6bf0ffd7ecfca5e4388c55205dda54001
Veronika <Veronika@DESKTOP-S01858F>
2020-04-16 17:26:47 +0300

    Added function, stored procedure and transaction

---
+++ Advanced/FUNCTION.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION fnIsPossibleSupply(@FlowerId INT, @PlantationId INT, @Amount INT)
+	RETURNS BIT
+AS
+	BEGIN
+		DECLARE @AmountFlowerOnPlantation INT;
+		DECLARE @RESULT BIT;
+		SET @AmountFlowerOnPlantation = (SELECT PlantationFlower.Amount FROM PlantationFlower 
+			WHERE   @PlantationId = PlantationFlower.PlantationId AND  PlantationFlower.FlowerId = @FlowerId);
+			IF (@AmountFlowerOnPlantation > @Amount)
+				SET @RESULT = 0;
+			ELSE
+				SET @RESULT = 1;
+		RETURN @RESULT;
+				
+	END
+GO
